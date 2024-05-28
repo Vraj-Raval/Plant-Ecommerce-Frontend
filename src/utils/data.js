bf08cd762709818f6a5fac207a8616c764da9b4e
Vraj-Raval <ravalvraj3000@gmail.com>
2024-05-29 01:05:46 +0530

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,52 @@
+export const category = [
+  {
+    img: "https://images.unsplash.com/photo-1567225557594-88d73e55f2cb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Indoor Plants",
+    off: "20-40% OFF",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1582131503261-fca1d1c0589f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Plants with vase",
+    off: "10-20% OFF",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597188447710-16db269b9dd1?q=80&w=1893&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Outdoor Plants",
+    off: "20-40% OFF",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533792344354-ed5e8fc12494?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Herbs",
+    off: "30-40% OFF",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533038590840-1cde6e668a91?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Ivy Plants",
+    off: "10-40% OFF",
+  },
+];
+
+export const filter = [
+  {
+    name: "Product Categories",
+    value: "category",
+    items: [
+      "Indoor",
+      "Outdoor",
+      "Plants with vase",
+      "Herbs",
+      "Ivy",
+      
+    ],
+  },
+  {
+    name: "Filter by Price",
+    value: "price",
+    items: [],
+  },
+  // {
+  //   name: "Filter by Size",
+  //   value: "size",
+  //   items: ["S", "M", "L", "XL", "XXL"],
+  // },
+];
